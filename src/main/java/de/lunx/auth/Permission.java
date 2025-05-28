42d87a2d0685eae2d80152dc9da88e7763af36f5
Julian Weinelt <julian.weinelt@outlook.de>
2025-05-28 12:43:24 +0200

    Added permissions

---
+++ src/main/java/de/lunx/auth/Permission.java
@@ -0,0 +1,16 @@
+package de.lunx.auth;
+
+public enum Permission {
+    GET_DATA,
+    CHANGE_DATA,
+    DELETE_DATA,
+    ADD_DATA,
+
+    CREATE_TABLE,
+    EDIT_TABLE,
+    DELETE_TABLE,
+
+    GRANT,
+
+    EXECUTE
+}
