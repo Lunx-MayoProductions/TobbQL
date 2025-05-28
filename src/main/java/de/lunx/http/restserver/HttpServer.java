2b342242ff9f714ab796e74e7c34ede36e2370e4
Julian Weinelt <julian.weinelt@outlook.de>
2025-05-28 12:42:35 +0200

    Reworked REST

--- src/main/java/de/lunx/http/restserver/HttpServer.java
+++ src/main/java/de/lunx/http/restserver/HttpServer.java
@@ -1,49 +1,48 @@
 package de.lunx.http.restserver;
 
+import com.auth0.jwt.interfaces.DecodedJWT;
 import com.google.gson.Gson;
 import com.google.gson.JsonElement;
+import de.lunx.Main;
 import de.lunx.auth.AuthManager;
 import de.lunx.data.DataManager;
-import de.lunx.querying.QueryParser;
 import io.javalin.Javalin;
+import io.javalin.http.HttpStatus;
 import lombok.extern.slf4j.Slf4j;
+
+import java.time.Instant;
+import java.util.Date;
 
 @Slf4j
 public class HttpServer {
-
+    private final HttpStatus unauthorized = HttpStatus.UNAUTHORIZED;
+    private final HttpStatus internalError = HttpStatus.INTERNAL_SERVER_ERROR;
+    private final HttpStatus badRequest = HttpStatus.BAD_REQUEST;
     private final Gson GSON = new Gson();
 
-    public void startServer(DataManager dataManager, AuthManager authManager) {
-        QueryParser parser = new QueryParser(dataManager);
-
+    public void startServer() {
         Javalin app = Javalin.create()
-                .post("/query", ctx -> {
-                    String user = ctx.queryParam("user");
-                    String pass = ctx.queryParam("pass");
-                    String query = ctx.queryParam("query");
-
-                    if (user == null || pass == null || query == null) {
-                        ctx.status(400);
-                        ctx.result("Missing user/pass/query");
+                .before(ctx -> {
+                    if (ctx.url().contains("auth")) return;
+                    String token = ctx.header("Authorization");
+                    if (token == null) {
+                        ctx.result(QueryError.error("Token missing",
+                                "Please provide a valid token to use the service."));
+                        ctx.status(unauthorized);
+                        ctx.skipRemainingHandlers();
+                        return;
                     }
-
-                    ctx.result(new Gson().toJson(authManager.authenticate(user, pass)
-                            .map(u -> {
-                                try {
-                                    JsonElement result = parser.parse(query);
-                                    return result.toString();
-                                } catch (Exception e) {
-                                    ctx.status(400);
-                                    return "Query error: " + e.getMessage();
-                                }
-                            })
-                            .orElseGet(() -> {
-                                ctx.status(403);
-                                return "Invalid credentials";
-                            })));
+                    token = token.replace("Bearer ", "");
+                    DecodedJWT decoded = Main.getInstance().getJwt().decode(token);
+                    if (decoded.getExpiresAt().before(Date.from(Instant.now()))) {
+                        ctx.result(QueryError.error("Token expired",
+                                "This token cannot be used. Please request a new one."));
+                        ctx.status(unauthorized);
+                        ctx.skipRemainingHandlers();
+                    }
                 })
-                .get("/query", context -> {
-                    context.result(GSON.toJson(authManager.listUsers()));
+                .post("/query", ctx -> {
+                    //TODO: Rework parsing
                 })
                 .start(25544);
         log.info("Started HTTP server on port 25544");
