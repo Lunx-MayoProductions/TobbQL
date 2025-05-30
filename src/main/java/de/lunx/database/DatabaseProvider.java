8c78dad6205056eb7c7389258e56909ef039b7d9
Lunx Irgendwas <YLunxY@outlook.com>
2025-05-30 10:06:09 +0200

    Removing Database Providers. How fucking dumb acctually am i? I AM BUILDING A DATABASE AND ADDING DB PROVIDERS

--- src/main/java/de/lunx/database/DatabaseProvider.java
+++
@@ -1,7 +0,0 @@
-package de.lunx.database;
-
-public interface DatabaseProvider {
-    void connect();
-    void disconnect();
-    void logRequest(String origin, String method, String path, String query, String body);
-}
